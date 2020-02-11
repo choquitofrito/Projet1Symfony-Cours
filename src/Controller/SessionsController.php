04b52aa80dadb60dcfa29357bc19331450b14ea6
choquito <zyriab@gmail.com>
2020-02-11 16:31:56 +0100

    sessions get et set ok

--- src/Controller/SessionsController.php
+++ src/Controller/SessionsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use DateTime;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,13 +12,44 @@ class SessionsController extends AbstractController
     /**
      * @Route ("/sessions/creer/variable/session")
      */
-    public function creerVariableSession (SessionInterface $objSession) {
-        // session_start
-        // $_SESSION['nom']='Lucy';
-        // session_close
-        $objSession->set('nom','Lucy');
-        return $this->render ('/sessions/creer_variable_session.html.twig');
-
+    public function creerVariableSession(SessionInterface $objSession)
+    {
+        $objSession->set('nom', 'Lucy');
+        return $this->render('/sessions/creer_variable_session.html.twig');
     }
 
+    /**
+     * @Route ("/sessions/afficher/variable/session")
+     */
+    public function afficherVariableSession(SessionInterface $objSession)
+    {
+        $nom = $objSession->get('nom');
+        return $this->render(
+            '/sessions/afficher_variable_session.html.twig',
+            ['nom' => $nom]
+        );
+    }
+
+    /**
+     * @Route ("/sessions/creer/variable/objet")
+     */
+    public function creerVariableObjet(SessionInterface $objSession)
+    {
+        $objDate = new DateTime();
+        $objSession->set('uneDate', $objDate);
+        return $this->render('/sessions/creer_variable_objet.html.twig');
+    }
+
+
+    /**
+     * @Route ("/sessions/afficher/variable/objet")
+     */
+    public function afficherVariableObjet(SessionInterface $objSession)
+    {
+        $objDate = $objSession->get('uneDate');
+        return $this->render(
+            '/sessions/afficher_variable_objet.html.twig',
+            ['uneDate' => $objDate]
+        );
+    }
 }
